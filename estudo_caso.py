9e3996f11506fe8b488025f9d509a42d3716c2f7
SandAyla <114297085+SandAyla@users.noreply.github.com>
2024-10-08 18:27:39 -0300

    Update estudo_caso.py

--- estudo_caso.py
+++ estudo_caso.py
@@ -1,22 +1,40 @@
 from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
 
 #Definicao da estrutura do modelo. Vinculos 
-model = BayesianNetwork([('H', 'D'), ('D', 'S'), ('D', 'E')])
+model = BayesianNetwork([('H', 'C'), ('I', 'C'), ('C', 'S')])
 
 #Criação das tabelas de probabilidade condicional
 #variable_card define o número de valores possíveis que a variável pode assumir
 #evidence_card faz a mesma coisa mas para a evidência (a quem a variavel depende)
 
-cpd_h = TabularCPD(variable='H', variable_card=2, values=[[0.2], [0.8]])
+cpd_h = TabularCPD(variable='H', variable_card=2, values=[[0.15], [0.85]])
 
-cpd_d = TabularCPD(variable='D', variable_card=2, values=[[0.1, 0.01],[0.9, 0.99]], evidence=['H'], evidence_card=[2])
+cpd_i = TabularCPD(variable='I', variable_card=2, values=[[0.46], [0.54]])
 
-cpd_s = TabularCPD(variable='S', variable_card=2, values=[[0.8, 0.1],[0.2, 0.9]], evidence=['D'], evidence_card=[2])
+cpd_c = TabularCPD(variable='C', 
+                   variable_card=2, 
+                   values=[[0.99, 0.96, 0.95, 0.85 ], 
+                           [0.01, 0.04, 0.05, 0.15 ]], 
+                   evidence=['H', 'I'], 
+                   evidence_card=[2, 2])
 
-cpd_e = TabularCPD(variable='E', variable_card=2, values=[[0.9, 0.05],[0.1, 0.95]], evidence=['D'], evidence_card=[2])
+
+cpd_s = TabularCPD(variable='S', variable_card=2, values=[[0.09, 0.0], [0.91, 1]], evidence=['C'], evidence_card=[2])
 
 #Associando CPD ao modelo
-model.add_cpds(cpd_h, cpd_d, cpd_s, cpd_e)
+model.add_cpds(cpd_h, cpd_i, cpd_c, cpd_s)
 
 print(model.get_cpds('S'))
+
+
+
+# Realizando a inferência no modelo
+inferencia = VariableElimination(model)
+
+# Consultando a probabilidade conjunta P(H, I, C, S)
+prob = inferencia.query(variables=['H'], evidence={'H': 1})
+
+print(prob)
+
